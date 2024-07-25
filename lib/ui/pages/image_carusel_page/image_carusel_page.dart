78edcce679c161306d88e5a7ae3b2ae1cfdb06ee
ccitygril <luganskaymasha@mail.ru>
2024-07-25 13:44:47 +0300

    add export file

---
+++ lib/ui/pages/image_carusel_page/image_carusel_page.dart
@@ -0,0 +1,3 @@
+export 'image_carusel_page_model.dart';
+export 'image_carusel_page_widget.dart';
+export 'image_carusel_page_wm.dart';
---
+++ lib/ui/pages/image_grid_page/image_grid_page.dart
@@ -0,0 +1,3 @@
+export 'image_grid_page_model.dart';
+export 'image_grid_page_widget.dart';
+export 'image_grid_page_wm.dart';
